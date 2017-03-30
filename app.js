557b87d2335f9c1a21ae22eb02ba958ab611cd41
SMuyesser <smuyesser@gmail.com>
2017-03-30 13:58:55 -0700

    added single state, state modifier function, render function, event listener

---
+++ app.js
@@ -0,0 +1,20 @@
+var state = {
+	items: []
+};
+
+var addItem = function(state, item) {
+	state.items.push(item)
+};
+
+var renderList = function(state, element) {
+	var itemsHTML = state.items.map(function(item) {
+		return '<li>' + item + '</li>';
+	});
+	element.html(itemsHTML);
+};
+
+$('.js-shopping-list-form').submit(function(event) {
+	event.preventDefault();
+	addItem(state, $('.shopping-list-entry').val());
+	renderList(state, $('.shopping-list'));
+});
